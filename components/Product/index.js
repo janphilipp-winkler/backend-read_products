48fd0b69366fab119c1aa24803b4469ab74a4b87
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2024-02-09 11:44:31 +0100

    solved challenge

---
+++ components/Product/index.js
@@ -0,0 +1,33 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import { StyledLink } from "../Link/Link.styled";
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading } = useSWR(`/api/products/${id}`);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      <p>Review Text: {data.reviews.map((review) => review.text)}</p>
+      <p>Rating: {data.reviews.map((review) => review.rating)}</p>
+
+      <StyledLink href="/">Back to all</StyledLink>
+    </ProductCard>
+  );
+}
